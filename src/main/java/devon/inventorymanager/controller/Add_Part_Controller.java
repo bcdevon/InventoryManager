3408a890aca53f1a4d1cccb9e0246624cd2a917a
bcdevon <brady.devon0@gmail.com>
2023-04-16 12:29:51 -0700

    can change between main add/modify part

--- src/main/java/devon/inventorymanager/controller/Add_Part_Controller.java
+++ src/main/java/devon/inventorymanager/controller/Add_Part_Controller.java
@@ -1,12 +1,18 @@
 package devon.inventorymanager.controller;
 
+import devon.inventorymanager.Main;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -34,6 +40,12 @@ public class Add_Part_Controller implements Initializable {
     public void onpartSave(ActionEvent actionEvent) {
     }
 
-    public void onpartCancel(ActionEvent actionEvent) {
+    public void onpartCancel(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("tables");
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/devon/inventorymanager/controller/Modfiy_Part_Controller.java
+++ src/main/java/devon/inventorymanager/controller/Modfiy_Part_Controller.java
@@ -1,11 +1,20 @@
 package devon.inventorymanager.controller;
 
+import devon.inventorymanager.Main;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,6 +23,7 @@ public class Modfiy_Part_Controller implements Initializable {
     public RadioButton modifyinHouse;
     public RadioButton modifyOutsourced;
     public Label modifypartID;
+    public Button modifyPartCancel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -26,5 +36,15 @@ public class Modfiy_Part_Controller implements Initializable {
 
     public void onModifyOutsourced(ActionEvent actionEvent) {
         modifypartID.setText("Company Name");
+    }
+
+    public void onModifyPartCancel(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("tables");
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
