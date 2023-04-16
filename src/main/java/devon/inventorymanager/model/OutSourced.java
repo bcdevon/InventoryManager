2c9fccbccc7f6bdad5a9239b8fc3a94d0fbe2753
bcdevon <brady.devon0@gmail.com>
2023-04-16 14:14:56 -0700

    data models except inventory

--- src/main/java/devon/inventorymanager/model/OutSourced.java
+++ src/main/java/devon/inventorymanager/model/OutSourced.java
@@ -1,4 +1,18 @@
 package devon.inventorymanager.model;
 
-public class OutSourced {
+public class OutSourced extends Part{
+    private String companyName;
+
+    public OutSourced(int id, String name, double price, int stock, int min, int max, String companyName) {
+        super(id, name, price, stock, min, max);
+        this.companyName = companyName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
 }
--- src/main/java/devon/inventorymanager/model/Product.java
+++ src/main/java/devon/inventorymanager/model/Product.java
@@ -1,4 +1,67 @@
 package devon.inventorymanager.model;
 
 public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int stock;
+    private int min;
+    private int max;
+
+    public Product(int id, String name, double price, int stock, int min, int max) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.min = min;
+        this.max = max;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
 }
