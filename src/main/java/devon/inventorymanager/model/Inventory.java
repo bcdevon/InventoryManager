8c02027a131c139f6697c9a738649069e97a4f62
bcdevon <brady.devon0@gmail.com>
2023-04-16 19:33:04 -0700

    added data in both tables using inventory class

--- src/main/java/devon/inventorymanager/model/Inventory.java
+++ src/main/java/devon/inventorymanager/model/Inventory.java
@@ -1,4 +1,22 @@
 package devon.inventorymanager.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class Inventory {
+    public static ObservableList<Part> allParts = FXCollections.observableArrayList();
+    private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
+    public static void addPart(Part part){
+        allParts.add(part);
+    }
+    public static void addProduct(Product product){
+        allProducts.add(product);
+    }
+    public static ObservableList<Part> getAllParts() {
+        return allParts;
+    }
+
+    public static ObservableList<Product> getAllProducts() {
+        return allProducts;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,6 @@ module devon.inventorymanager {
     exports devon.inventorymanager;
     exports devon.inventorymanager.controller;
     opens devon.inventorymanager.controller to javafx.fxml;
+    opens devon.inventorymanager.model to javafx.fxml;
+    exports devon.inventorymanager.model;
 }
