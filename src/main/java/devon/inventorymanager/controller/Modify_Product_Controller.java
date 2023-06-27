32d63000714ba9b2270e2f25827cffa1ba98dacd
bcdevon <brady.devon0@gmail.com>
2023-06-26 19:36:09 -0700

    modify product exceptions handled

--- src/main/java/devon/inventorymanager/controller/Modify_Product_Controller.java
+++ src/main/java/devon/inventorymanager/controller/Modify_Product_Controller.java
@@ -11,10 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
@@ -94,15 +91,78 @@ public class Modify_Product_Controller implements Initializable {
     }
 
     public void onSaveModifyProduct(ActionEvent actionEvent) {
-
+        //disable productID modification
+        productIDTF.setDisable(true);
         //get values from textfields
         int productId = Integer.parseInt(productIDTF.getText());
         String productName = productNameTF.getText();
-        int productInventory = Integer.parseInt(productInvTF.getText());
-        double productPrice = Double.parseDouble(productPriceTF.getText());
-        int productMax = Integer.parseInt(productMaxTF.getText());
-        int productMin = Integer.parseInt(productMinTF.getText());
+        String productInventoryS = productInvTF.getText();
+        String productPriceS = productPriceTF.getText();
+        String productMaxS = productMaxTF.getText();
+        String productMinS = productMinTF.getText();
 
+        //if the name value is blank then the product will not be added
+        if(productName.isBlank()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setContentText("Please enter a name for the Part");
+            alert.showAndWait();
+            System.out.println("Name Value is Blank");
+            return;
+        }
+
+        // convert values to correct data type and catch exceptions if user entered wrong data type.
+        int productInventory = 0;
+        double productPrice = 0;
+        int productMax = 0;
+        int productMin = 0;
+        String error = "";
+
+        try{
+            error = "productInventory";
+            productInventory = Integer.parseInt(productInventoryS);
+            productPrice = Double.parseDouble(productPriceS);
+            productMax = Integer.parseInt(productMaxS);
+            productMin = Integer.parseInt(productMinS);
+
+            // number of product in inventory cannot be less than minimum
+            if(productMin >productInventory){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Dialog");
+                alert.setContentText("Number in inventory cannot be less than min.");
+                alert.showAndWait();
+                System.out.println("product inventory must be >= prodouctmin");
+                return;
+            }
+            // number in stock cannot be more than maximum
+            if(productInventory > productMax){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Dialog");
+                alert.setContentText("Number in inventory cannot be more than max.");
+                alert.showAndWait();
+                System.out.println("productInventory must be <= productmax");
+                return;
+            }
+            // price cannot be less than zero
+            if(productPrice < 0){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Dialog");
+                alert.setContentText("Price cannot be less than zero");
+                alert.showAndWait();
+                System.out.println("price cannot be negative");
+                return;
+            }
+        }
+
+        //handle number format exception
+        catch (NumberFormatException e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("error dialog");
+            alert.setContentText("Please enter a valid value for " + error + " field" );
+            alert.showAndWait();
+            System.out.println(error + " " + "wrong value entered!");
+            return;
+        }
         //save the new values from the textfields
         product.setId(productId);
         product.setName(productName);
@@ -112,10 +172,6 @@ public class Modify_Product_Controller implements Initializable {
         product.setMin(productMin);
         //add associated parts to product
         product.getAssociatedParts().setAll(productAssociatedParts);
-
-        //save the associated parts to the product
-
-
         //return to main screen
         try{
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
