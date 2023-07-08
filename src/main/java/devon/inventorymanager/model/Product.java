4ce32f259e9d36cfc741b3a6b3ec82b1997655c6
bcdevon <brady.devon0@gmail.com>
2023-07-08 15:18:15 -0700

    unique id for products

--- src/main/java/devon/inventorymanager/model/Product.java
+++ src/main/java/devon/inventorymanager/model/Product.java
@@ -3,16 +3,49 @@ package devon.inventorymanager.model;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
     private ObservableList<Part> associatedParts;
     private int id;
+    private static int lastAssignedProductId = 0;
+    private static List<Integer> usedProductIds = new ArrayList<>();
+    //generate an id for each product added ids should be incremented by 1 each time and be unique
     public Product() {
         this.id = generateNewProductId();
     }
-
-    private static int lastAssignedProductId = 0;
     public static int generateNewProductId() {
-        return ++ lastAssignedProductId;
+        lastAssignedProductId = findNextProductId();
+        usedProductIds.add(lastAssignedProductId);
+        return lastAssignedProductId;
+    }
+    //increment 1 from the last assigned product id if the id is a duplicate increment by 1 until it is unique.
+    private static int findNextProductId(){
+        int nextProductId = lastAssignedProductId +1;
+        while (isDuplicateId(nextProductId)){
+            nextProductId++;
+        }
+        return nextProductId;
+    }
+    //If the id is a duplicate return true
+    private static boolean isDuplicateId(int id) {
+        for (int i = 0; i < usedProductIds.size(); i++) {
+            int usedProductId = usedProductIds.get(i);
+            if (usedProductId == id) {
+                return true;
+            }
+        }
+        // if the id is a duplicate of the test data return true
+        ObservableList<Product> allProducts = Inventory.getAllProducts();
+        for (int i = 0; i < allProducts.size(); i++) {
+            Product product = allProducts.get(i);
+            if (product.getId() == id) {
+                return true;
+            }
+        }
+
+        return false;
     }
     private String name;
     private double price;
