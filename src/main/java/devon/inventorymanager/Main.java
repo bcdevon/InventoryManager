40b8b69fe323c5d67452356d61b01de5cc91c830
bcdevon <brady.devon0@gmail.com>
2023-08-14 20:24:03 -0700

    main javadocs done

--- src/main/java/devon/inventorymanager/Main.java
+++ src/main/java/devon/inventorymanager/Main.java
@@ -6,16 +6,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-/** This class is the beginning of the application*/
+/** This Main class is the beginning of the application.
+ * This class initializes and launches the application.*/
 
 public class Main extends Application {
 
     /**This is the main method.
      * This is the first method that gets called when the application is launched*/
     public static void main(String[] args) {
+        //Add test data to the inventory
         Inventory.addTestData();
+        //Launch the application
         launch();
     }
 
@@ -24,10 +26,15 @@ public class Main extends Application {
      * @param stage This is the primary stage
      * @throws IOException If and input/output error occurs*/
     public void start(Stage stage) throws IOException {
+        // Load the main fxml file
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
+
+        //Create a scene and set it to the stage
         Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
         stage.setTitle("Main Screen");
         stage.setScene(scene);
+
+        //show the stage
         stage.show();
     }
 
