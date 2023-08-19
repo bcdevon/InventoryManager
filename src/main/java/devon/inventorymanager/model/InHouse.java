db6c075ddc12c9f811b6c5a9e4bbf650f15a57a4
bcdevon <brady.devon0@gmail.com>
2023-08-19 14:58:17 -0700

    javadocs comments inhouse and outsourced

--- src/main/java/devon/inventorymanager/model/InHouse.java
+++ src/main/java/devon/inventorymanager/model/InHouse.java
@@ -1,17 +1,36 @@
 package devon.inventorymanager.model;
-//constructor for inhouse part with getter and setter
+
+/**This is the InHouse class
+ * The InHouse class is a class that extends from the Part class
+ * It represents an InHouse part in the inventory*/
 public class InHouse extends Part {
+    //The machine ID of the part
     private int machineID;
 
+    /**This is the constructor for the InHouse class
+     * this constructor creates a new InHouse part
+     * @param stock Current inventory stock level of the InHouse part
+     * @param price Price of the part
+     * @param name Name of the part
+     * @param min Minimum inventory stock level allowed for the part
+     * @param max Maximum inventory stock level allowed for the part
+     * @param id Unique ID of the part
+     * @param machineID machine id of the part*/
     public InHouse(int id, String name, double price, int stock, int min, int max, int machineID) {
         super(id, name, price, stock, min, max);
         this.machineID = machineID;
     }
 
+    /**this is the getMachineID method
+     * This gets the machine ID of an InHouse Part
+     * @return machineID*/
     public int getMachineID() {
         return machineID;
     }
 
+    /**This is the setMachineID method
+     * This sets the machineID of the Inhouse part
+     * @param machineID */
     public void setMachineID(int machineID) {
         this.machineID = machineID;
     }
