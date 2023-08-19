0de86b579662d5de8a058c513422123ed47860d5
bcdevon <brady.devon0@gmail.com>
2023-08-19 11:13:54 -0700

    comments javadocs product.java

--- src/main/java/devon/inventorymanager/model/Product.java
+++ src/main/java/devon/inventorymanager/model/Product.java
@@ -15,38 +15,52 @@ public class Product {
     /**creates a new product with a generated unique ID.*/
     public Product() {
         this.id = generateNewProductId();
+
     /**Generates a new unique product ID.
      *@return The generated product ID*/
     }
     public static int generateNewProductId() {
+        //find the next available ID
         lastAssignedProductId = findNextProductId();
+        //add new ID to list of used ID's
         usedProductIds.add(lastAssignedProductId);
+        //return the new ID
         return lastAssignedProductId;
     }
     /**Finds the next available product ID that is not already used.
      * @return the next available product ID.*/
     private static int findNextProductId(){
+        //increment the last assigned ID by 1
         int nextProductId = lastAssignedProductId +1;
+        //check if the incremented ID has already been used
         while (isDuplicateId(nextProductId)){
+            //if ID has already been used increment by 1 and check again
             nextProductId++;
         }
+        //return the unique ID
         return nextProductId;
     }
     /**Checks if the given ID is a duplicate.
      * @param id  The ID to check for being a duplicate.
      * @return True if the ID is a duplicate, or else return false.*/
     private static boolean isDuplicateId(int id) {
+        //loop through all the product ID's that have been used
         for (int i = 0; i < usedProductIds.size(); i++) {
             int usedProductId = usedProductIds.get(i);
+            //check if ID being assigned has already been used.
             if (usedProductId == id) {
+                //if ID already used return true
                 return true;
             }
         }
-        // if the id is a duplicate of the test data return true
+        // check if the id is a duplicate of the test data
         ObservableList<Product> allProducts = Inventory.getAllProducts();
+        //loop through all the products
         for (int i = 0; i < allProducts.size(); i++) {
             Product product = allProducts.get(i);
+            //check if the products ID match the given ID
             if (product.getId() == id) {
+                // return true if ID is a duplicate
                 return true;
             }
         }
@@ -59,13 +73,22 @@ public class Product {
     private int min;
     private int max;
 
+    /**constructor to create a new product
+     * @param id unique ID of the product
+     * @param max maximum inventory stock level allowed for the product
+     * @param min minimum inventory stock level allowed for the product
+     * @param name name of the product
+     * @param price price of the product
+     * @param stock current inventory stock level of the product*/
     public Product(int id, String name, double price, int stock, int min, int max) {
+        //set the values of the product using the provided values
         this.id = id;
         this.name = name;
         this.price = price;
         this.stock = stock;
         this.min = min;
         this.max = max;
+        //initialize the associatedParts list as and ObservableList
         associatedParts = FXCollections.observableArrayList();
     }
 
