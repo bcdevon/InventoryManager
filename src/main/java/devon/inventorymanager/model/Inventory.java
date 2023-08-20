ac8aa9fb13c5d266808452c7e4175ceb97f94d12
bcdevon <brady.devon0@gmail.com>
2023-08-19 19:17:00 -0700

    inventory javadocs

--- src/main/java/devon/inventorymanager/model/Inventory.java
+++ src/main/java/devon/inventorymanager/model/Inventory.java
@@ -2,29 +2,54 @@ package devon.inventorymanager.model;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-/** This class creates and inventory of parts and products.*/
+/**This is the Inventory class.
+ * This class creates and manages an inventory of parts and products.*/
 public class Inventory {
+    //ObservableList to store all the Parts
     public static ObservableList<Part> allParts = FXCollections.observableArrayList();
+
+    //Observable list to store all the Products
     private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
+
+    /**This is the addPart method.
+     * this method adds a new part to the allParts list.
+     * @param part The part to be added.*/
     public static void addPart(Part part){
         allParts.add(part);
     }
-    public static void addProduct(Product product){
-        allProducts.add(product);
-    }
+
+    /**This is the addProduct method.
+     * this method add a new product to the allProducts list.
+     * @param product The product to be added.*/
+    public static void addProduct(Product product){ allProducts.add(product); }
+
+    /**This is the deletePart Method.
+     * this method deletes a part from the inventory.
+     * @param part part to be deleted*/
     public static void deletePart(Part part){
         getAllParts().remove(part);
     }
-    public static void deleteProduct(Product product) {getAllProducts().remove(product);}
-    public static ObservableList<Part> getAllParts() {
-        return allParts;
-    }
 
+    /**This is the deleteProduct method.
+     * this method deletes a product from the inventory.
+     * @param product product to be deleted*/
+    public static void deleteProduct(Product product) {getAllProducts().remove(product);}
+
+    /**This is the getAllParts method.
+     * this method gets a list of all the parts in the inventory.
+     * @return ObservableList of all parts*/
+    public static ObservableList<Part> getAllParts() {return allParts;}
+
+    /**This is the getAllProducts method.
+     * this method gets a list of all the products in the inventory.
+     * @return ObservableList of all products*/
     public static ObservableList<Product> getAllProducts() {
         return allProducts;
     }
-    //sample data for parts and products
-    /** This method adds test data to the inventory*/
+
+    /** This is the addTestData method
+     * this method creates parts and products and is used
+     * to populate the inventory with sample data*/
     public static void addTestData() {
         InHouse wheel = new InHouse(1, "Wheel", 25.00, 5, 1, 10, 42);
         Inventory.addPart(wheel);
--- src/main/java/devon/inventorymanager/model/OutSourced.java
+++ src/main/java/devon/inventorymanager/model/OutSourced.java
@@ -1,34 +1,34 @@
 package devon.inventorymanager.model;
 
-/**This is the OutSourced class
+/**This is the OutSourced class.
  * The OutSourced class is a class that extends from the Part class
- * It represents an Outsourced part in the inventory*/
+ * It represents an Outsourced part in the inventory.*/
 public class OutSourced extends Part{
     //The name of the company the part is outsourced from
     private String companyName;
 
-    /**This is the constructor for the OutSourced class
-     * This constructor creates a new Outsourced part
-     * @param id Unique ID of the outsourced part
-     * @param max Maximum inventory stock level allowed for the Outsourced part
-     * @param min Minimum inventory stock level allowed for the Outsourced part
-     * @param name Name of the Outsourced part
-     * @param price Price of the Outsourced part
-     * @param stock Current inventory stock level of the Outsourced part*/
+    /**This is the constructor for the OutSourced class.
+     * This constructor creates a new Outsourced part.
+     * @param id Unique ID of the outsourced part.
+     * @param max Maximum inventory stock level allowed for the Outsourced part.
+     * @param min Minimum inventory stock level allowed for the Outsourced part.
+     * @param name Name of the Outsourced part.
+     * @param price Price of the Outsourced part.
+     * @param stock Current inventory stock level of the Outsourced part.*/
     public OutSourced(int id, String name, double price, int stock, int min, int max, String companyName) {
         super(id, name, price, stock, min, max);
         this.companyName = companyName;
     }
 
-    /**This is the getCompanyName method
-     * This gets the name of the company associated with the outsourced part
+    /**This is the getCompanyName method.
+     * This gets the name of the company associated with the outsourced part.
      * @return companyName*/
     public String getCompanyName() {
         return companyName;
     }
 
-    /**This is the setCompanyName method
-     * This method sets the name of the company associated with the outsourced part
+    /**This is the setCompanyName method.
+     * This method sets the name of the company associated with the outsourced part.
      * @param companyName*/
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
